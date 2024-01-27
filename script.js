f3c2475c5c9f874223f0c2da884e87519879facd
unknown <ganarez03@gmail.com>
2024-01-27 16:20:55 +0700

    tambah operasi matematika dan fixing bug display

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+let currentOperation = ''
+
 function appendToDisplay(value){
+    if((value === '*' || value === '/' || value === '+') && document.getElementById('display').innerText === ''){
+        return
+    }
+
+    if (value === '*' || value === '/' || value === '+'){
+        currentOperation = value
+    }
     document.getElementById('display').innerText += value
 }
 
 function clearDisplay(){
     document.getElementById('display').innerText = ''
 }
+
+function deleteNum() {
+    let currentDisplay = document.getElementById('display').innerText;
+    document.getElementById('display').innerText = currentDisplay.slice(0, -1);
+}
+
+function operation(){
+    try{
+        const result = eval(document.getElementById('display').innerText)
+        document.getElementById('display').innerText = result
+    }
+    catch{
+        document.getElementById('display').value = eror
+    }
+}
+
